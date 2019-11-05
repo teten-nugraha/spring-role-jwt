4cab1ca818ba859a17f6f0f75f706879ace2138e
Teten <teten.nugraha18@gmail.com>
2019-11-05 09:47:12 +0700

    first

---
+++ src/main/java/com/jwt/jwtrole/JwtRoleApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.jwtrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRoleApplication.class, args);
+    }
+
+}
